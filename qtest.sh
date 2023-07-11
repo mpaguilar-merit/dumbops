c048bf8e705d493ecb701f99e663f499eff5218b
Michael Aguilar <michael@meritpages.com>
2023-07-11 11:11:55 -0500

    Env dumping

--- qtest.sh
+++ qtest.sh
@@ -11,8 +11,29 @@ echo "Short commit hash: $COMMIT_HASH"
 
 echo "Github ref name: $GITHUB_REF_NAME"
 
+CONFIG_DIR=./configs/basic
+
 echo "Current directory: "
 pwd
 
 echo "Listing files:"
 ls -al
+
+mv app build
+
+cd build
+
+echo "Listing build dir:"
+ls -al
+
+echo "Listing config dir:"
+ls -al $CONFIG_DIR
+
+# get utilities
+echo "Retrieving yq"
+wget -O $CONFIG_DIR/usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+chmod +x $CONFIG_DIR/usr/local/bin/yq
+echo ""
+
+
+
