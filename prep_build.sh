13167eb11ce5ea500da4f1259e382819ce25788e
Michael Aguilar <michael@meritpages.com>
2023-07-12 14:52:05 -0500

    Add to prep, rename prep

--- prep_build.sh
+++ prep_build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# tmp token: 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
+# exit on failure
+set -e
 
-echo "This is a quick test"
+echo "This is a test"
 echo "Target ruby version: $TARGET_RUBY_VER"
 echo "Github SHA: $GITHUB_SHA"
 
@@ -21,6 +22,7 @@ ls -al
 
 mv app build
 
+echo "Changing to build directory"
 cd build
 
 echo "Listing build dir:"
@@ -33,9 +35,12 @@ ls -al $CONFIG_DIR
 echo "Retrieving yq"
 # just in case it doesn't exist
 mkdir -p $CONFIG_DIR/usr/local/bin
-wget -O $CONFIG_DIR/usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+wget --no-verbose -O $CONFIG_DIR/usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
 chmod +x $CONFIG_DIR/usr/local/bin/yq
+echo "Done"
 echo ""
 
+IMG_TAG="$TARGET_RUBY_VER:$COMMIT_HASH"
 
-
+# set the image tag in the global environment so the docker step can find it
+echo "IMG_TAG=$IMG_TAG" >> "$GITHUB_ENV"
